ca49411936f35fa8065a6caacc132de3e15be0fc
Dhiliban <dhiliban.baskar@gmail.com>
2024-06-07 20:21:20 +0530

    import removed

--- src/test/java/com/starAgile/testng/ReadExcel.java
+++ src/test/java/com/starAgile/testng/ReadExcel.java
@@ -12,8 +12,6 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class ReadExcel {
